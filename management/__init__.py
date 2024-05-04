537d8b094378cce47d1f63793ad9fe58fe4484e8
isaacmayela <isaacmayela27@gmail.com>
2024-05-04 11:46:07 +0200

    deploying

--- management/__init__.py
+++ management/__init__.py
@@ -1,17 +1,17 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-import os
+# from django.core.management.base import BaseCommand
+# from django.contrib.auth.models import User
+# import os
 
-class Command(BaseCommand):
-    help = 'Create a superuser'
+# class Command(BaseCommand):
+#     help = 'Create a superuser'
 
-    CREDENTIALS = os.environ.get("SUPERUSER_CREDENTIALS").split(",")
+#     CREDENTIALS = os.environ.get("SUPERUSER_CREDENTIALS").split(",")
 
-    # print(CREDENTIALS)
+#     # print(CREDENTIALS)
 
-    def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('username', self.CREDENTIALS[0], self.CREDENTIALS[1])
-            self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
-        else:
-            self.stdout.write(self.style.SUCCESS('Superuser already exists'))
+#     def handle(self, *args, **kwargs):
+#         if not User.objects.filter(username='admin').exists():
+#             User.objects.create_superuser('username', self.CREDENTIALS[0], self.CREDENTIALS[1])
+#             self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+#         else:
+#             self.stdout.write(self.style.SUCCESS('Superuser already exists'))
