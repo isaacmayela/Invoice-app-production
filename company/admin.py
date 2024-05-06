a95224a135520f4fc697c65075e22aa94b5390c6
isaacmayela <isaacmayela27@gmail.com>
2024-05-06 10:22:35 +0200

    feat: add company routes

--- company/admin.py
+++ company/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company, Customer, Invoice, Article
 
 # Register your models here.
 admin.site.register(Company)
+admin.site.register(Customer)
+admin.site.register(Invoice)
+admin.site.register(Article)
