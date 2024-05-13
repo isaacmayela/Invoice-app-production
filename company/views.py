bda55bb642bb7d3a7b3a18a8ef4bf1de755106f3
isaacmayela <isaacmayela27@gmail.com>
2024-05-13 02:37:36 +0200

    refactor: refactoring field id_number

--- company/views.py
+++ company/views.py
@@ -220,6 +220,7 @@ class InvoiceInfosView(EditorPermissionsMixins, generics.GenericAPIView, mixins.
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
     permission_classes = [IsAuthenticated]
+    lookup_field = "pk"
 
     def get_queryset(self):
         id_number = self.kwargs.get('id_number')
